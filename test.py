502232ccb606c0065b9ba2fa277ebd8e69bb0330
pzc <pzc@qq.com>
2018-01-17 18:08:39 +0800

    wangyiyun

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #—*—coding=utf-8
-
+import urllib2
+import sys
+'''
 import requests
 import sys
 reload(sys)
@@ -8,11 +10,18 @@ from bs4 import BeautifulSoup
 url1 = 'http://music.163.com/discover/toplist?id=3778678'#云音乐热歌榜
 response = requests.get(url1)
 print response.status_code
-html = response.text
+html = response.content
 fo = open ('a.log','w')
 sys.stdout = fo
-print html
-soup = BeautifulSoup(html,'lxml')
+print html '''
+#soup = BeautifulSoup(html)
+#sss = soup.find_all('ul')
 #items = soup.find('iframe')
 
 #a = items.find('body')
+url1 = 'http://music.163.com/discover/toplist?id=3778678'
+request = urllib2.Request(url1)
+response =urllib2.urlopen(request)
+fo = open ('a.log','w')
+sys.stdout = fo
+print response.read()
--- wangyiyun.py
+++ wangyiyun.py
@@ -1,34 +1,61 @@
 #—*—coding=utf-8
 
-import re  
-import requests  
-import json  
-import sys 
+import requests
+import sys
+import json
+import time
 reload(sys)
-sys.setdefaultencoding('utf-8')
-def get_all_hotsongs():  
-    """抓热搜榜所有歌曲"""  
-    url = 'http://music.163.com/discover/toplist?id=3778678'  
-    headers = {  
-        'Cookie':'__e_=1515461191756; _ntes_nnid=af802a7dd2cafc9fef605185da6e73fb,1515461190617; _ntes_nuid=af802a7dd2cafc9fef605185da6e73fb; JSESSIONID-WYYY=HMyeRdf98eDm%2Bi%5CRnK9iB%5ChcSODhA%2Bh4jx5t3z20hhwTRsOCWhBS5Cpn%2B5j%5CVfMIu0i4bQY9sky%5CsvMmHhuwud2cDNbFRD%2FHhWHE61VhovnFrKWXfDAp%5CqO%2B6cEc%2B%2BIXGz83mwrGS78Goo%2BWgsyJb37Oaqr0IehSp288xn5DhgC3Cobe%3A1515585307035; _iuqxldmzr_=32; __utma=94650624.61181594.1515583507.1515583507.1515583507.1; __utmc=94650624; __utmz=94650624.1515583507.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=94650624.4.10.1515583507',  
-        'Host':'music.163.com',  
-        'Refere':'http://music.163.com/',  
-        'Upgrade-Insecure-Requests':'1',  
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'  
-    }  
-    r = requests.get(url,headers=headers)  
-  
-    #使用正则表达式匹配正文响应  
-    reg1 = r'<ul class="f-hide"><li><a href="/song\?id=\d*?">.*</a></li></ul>'  
-    result_contain_songs_ul = re.compile(reg1).findall(r.text)  
-    result_contain_songs_ul = result_contain_songs_ul[0]  
-  
-    reg2 = r'<li><a href="/song\?id=\d*?">(.*?)</a></li>'  
-    reg3 = r'<li><a href="/song\?id=(\d*?)">.*?</a></li>'  
-    hot_songs_name = re.compile(reg2).findall(result_contain_songs_ul)  
-    hot_songs_id = re.compile(reg3).findall(result_contain_songs_ul)  
-  
-    #返回歌曲名 歌曲id  
-    for i in hot_songs_name:
-        print i.decode('utf-8').encode('gbk','ignore')
-get_all_hotsongs()
+sys.setdefaultencoding('utf8')
+from bs4 import BeautifulSoup
+url1 = 'http://music.163.com/discover/toplist?id=3778678'#云音乐热歌榜
+#UA必須要設置
+headers = {  
+	'Cookie':'__e_=1515461191756; _ntes_nnid=af802a7dd2cafc9fef605185da6e73fb,1515461190617; _ntes_nuid=af802a7dd2cafc9fef605185da6e73fb; JSESSIONID-WYYY=HMyeRdf98eDm%2Bi%5CRnK9iB%5ChcSODhA%2Bh4jx5t3z20hhwTRsOCWhBS5Cpn%2B5j%5CVfMIu0i4bQY9sky%5CsvMmHhuwud2cDNbFRD%2FHhWHE61VhovnFrKWXfDAp%5CqO%2B6cEc%2B%2BIXGz83mwrGS78Goo%2BWgsyJb37Oaqr0IehSp288xn5DhgC3Cobe%3A1515585307035; _iuqxldmzr_=32; __utma=94650624.61181594.1515583507.1515583507.1515583507.1; __utmc=94650624; __utmz=94650624.1515583507.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=94650624.4.10.1515583507',  
+	'Host':'music.163.com',  
+	'Refere':'http://music.163.com/',  
+	'Upgrade-Insecure-Requests':'1',  
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'  
+}  
+response = requests.get(url1,headers=headers)
+print response.status_code
+html = response.text
+
+soup = BeautifulSoup(html,'lxml')
+update_time = soup.find('span',attrs={'class':'sep s-fc3'}).text
+print update_time
+'''
+goaltext = soup.find('ul',class_="f-hide")
+titles =goaltext.find_all('li')
+i = 1
+for title in titles:
+	print i,'.',title.text.decode('utf-8').encode('gbk','ignore')
+	i +=1
+'''
+#items = soup.find('iframe')
+textarea = soup.find('textarea').text
+i = 1
+contents = json.loads(str(textarea))
+fo = open('test.log','w')
+sys.stdout = fo
+for a in range(195):
+	t1 = time.localtime(contents[a].get('publishTime')/1000)
+	t2 = time.strftime("%Y-%m-%d %H:%M:%S",t1)
+	
+	t3 = contents[a].get('duration')/1000
+	min = str(t3/60)
+	sec = str(t3%60)
+	if len(sec)<2:
+		sec = '0'+str(sec)
+	artist = contents[a].get('artists')[0].get('name')
+	music_name = contents[a].get('name')
+	album = contents[a].get('album').get('name')
+	print i,'.',music_name,u' 播放时长：',min+':'+str(sec)#.encode('gbk','ignore')
+	print u'歌手：',artist
+	print u'专辑：',album
+	if contents[a].get('alias'):
+		alias = contents[a].get('alias')[0]
+		print alias
+	print u'发行时间：',t2
+	i += 1
+	print'--------------------------------------------------------------------'
+#a = items.find('body')
