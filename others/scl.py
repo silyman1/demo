c6cdefff54df50dbefb5d581c0656615d3954710
1161626597 <1161626597@qq.com>
2019-03-01 09:19:26 +0800

    gyscl

--- others/scl.py
+++ others/scl.py
@@ -8,6 +8,8 @@ sys.setdefaultencoding('utf-8')
 import codecs
 import unicodecsv
 '''http://www.iwt.cn/CN/article/showTenYearVolumnDetail.do?nian=1986'''
+
+headers = { 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4)AppleWebKit/537.36(KHTML,likeGecko)Chrome/44.0.2403.157 Safari/537.36',    'Connection':'keep-alive','Accept-Encoding':'gzip, deflate'}
 def get_qikang_list(url):
 	urls_list =[]
 	rep =requests.get(url)
@@ -31,7 +33,7 @@ def get_zhaiyao(url):
 	return urls_list
 def get_detail(url):
 	item_list =[]
-	rep =requests.get(url)
+	rep =requests.get(url,headers=headers)
 	rep.encoding = rep.apparent_encoding
 	soup = BeautifulSoup(rep.text,'lxml')
 	results = soup.find('td',attrs={'class':'J_author_cn'})
@@ -44,8 +46,11 @@ def get_detail(url):
 	l1 =name_list[::2]
 	l11 = []
 	for a in l1:
-		l11.append(a.replace(',',''))
-	print l1
+		oneone = a.split(',')
+		for one in oneone:
+			if one:
+				l11.append(one)
+	print 'l11:',l11
 	l2 = name_list[1::2]
 	nums =[]
 	for i in l2:
@@ -60,41 +65,95 @@ def get_detail(url):
 			company_list.append(result.string)
 	print results.parent.next_sibling.next_sibling.find('td').string
 	index = 0
+	someitems = get_detail2(rep.text)
+	# for someitem in someitems:
+		# item_list.append(someitem)
 	for item in l11:
-		name = item
-		company = ''
-		for num in nums: 
-			company += company_list[int(num)-1]
-		print name,':',company
-	goal=results.find_all('tr')
-	if goal == None:
-		return
-	for i in range(1,len(goal)):
 		myitem = []
-		item=goal[i].find_all('font',attrs={'color':'blue'})
-		author = '暂无'
-		company = '暂无'
-		mail= '暂无'
-		try:
-			author = item[0].string
-			print item[0].string
-		except:
-			author = '暂无'
-		try:
-			company = item[1].string
-			print item[1].string
-		except:
-			company = '暂无'
-		mail_pats = goal[i].find_all('a')
-		for pat in mail_pats:
-			if pat.get('href').startswith('mailto'):
-				mail= pat.string
-		print mail
-		myitem.append(author)
+		name = item.strip()
+		company = ''
+		# tel =''
+		# mymail = ''
+		if nums:
+			for num in nums: 
+				company += company_list[int(num)-1]
+		else:
+			if company_list:
+				company = company_list[0]
+		print name,':',company
+	# goal=results.find_all('tr')
+	# if goal == None:
+		# return
+	# for i in range(1,len(goal)):
+		# myitem = []
+		# item=goal[i].find_all('font',attrs={'color':'blue'})
+		# author = '暂无'
+		# company = '暂无'
+		# mail= '暂无'
+		# try:
+			# author = item[0].string
+			# print item[0].string
+		# except:
+			# author = '暂无'
+		# try:
+			# company = item[1].string
+			# print item[1].string
+		# except:
+			# company = '暂无'
+		# mail_pats = goal[i].find_all('a')
+		# for pat in mail_pats:
+			# if pat.get('href').startswith('mailto'):
+				# mail= pat.string
+		# print mail
+		myitem.append(name)
 		myitem.append(company)
-		myitem.append(mail)
+		for someitem in someitems:
+			if someitem[0].startswith(name):
+				myitem.append(someitem[1])
+				myitem.append(someitem[2])
 		item_list.append(myitem)
 	return item_list
+def get_detail2(rep):
+	# csvfile = file('gyscl2.csv','a')#ks.csv
+	# csvfile.write(codecs.BOM_UTF8)
+	someitems = []
+	soup = BeautifulSoup(rep)
+	results = soup.find_all("span",attrs={"class":"J_zhaiyao"})
+	if results == None:
+		return someitems
+	for result in results:
+		item =[]
+		if result.find('strong') == None:
+			continue
+		if result.find('strong').string == '通讯作者:' or result.find('strong').string == '作者简介':
+			# pat1 =re.compile('.*?E-mail:(.*?)')
+			# pat2 =re.compile('电话:(\d+),.*?E-mail')
+
+			for thing in result.stripped_strings:
+				if thing.find('电话:') ==-1 or thing.find('E-mail:') ==-1 or thing.startswith('E-mail:'):
+					continue
+				print 'thing:',thing
+				name =''
+				tel =''
+				mymail =''
+
+				goals = thing.split(',')
+				for i in range(len(goals)):
+					if i == 0:
+						name = goals[i].replace(':','').strip()
+					if goals[i].find('电话:') !=-1:
+						tel = goals[i].replace('电话:','').strip()
+					if goals[i].find('@') !=-1:
+						mymail = goals[i].replace('E-mail:','').strip()
+				print name
+				print tel
+				print mymail
+				item.append(name)
+				item.append(tel)
+				item.append(mymail)
+		someitems.append(item)
+		# write_csv(someitems,csvfile)
+	return someitems
 def write_csv(items,csvfile):
 	if items is None:
 		return
@@ -108,6 +167,7 @@ if __name__ == "__main__":
 	fo = open("gyscl.log",'w+')
 	s = sys.stdout
 	sys.stdout = fo
+	flag =True
 	for page in range(1981,2019):
 		origin_url = 'http://www.iwt.cn/CN/article/showTenYearVolumnDetail.do?nian='+str(page)
 		print origin_url
@@ -119,15 +179,16 @@ if __name__ == "__main__":
 			for zurl in zurls_list:
 				url = zurl.replace('..',astr)
 				print '=============',url
-			# if url == 'http://www.rubbertire.com.cn/gy/xjgy/ch/reader/view_abstract.aspx?file_no=LTY20161209&flag=1':
+			# if url == 'http://www.iwt.cn/CN/abstract/abstract15017.shtml':
 				# flag = False
 				# print 'go on'
 			# if flag:
 				# print 'pass'
 				# continue
-				url = 'http://www.iwt.cn/CN/abstract/abstract12047.shtml'
+				# url = 'http://www.iwt.cn/CN/abstract/abstract12047.shtml'
 				items_list = get_detail(url)
 				write_csv(items_list,csvfile)
 				print '++++++++++++++++++'
-				break
-			break
+				# break
+			# break
+		# break
